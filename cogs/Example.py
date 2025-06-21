289199ed7f1801133a0543b160c8177ec24a8c5b
easytopy <juncom4261@gmail.com>
2025-06-21 22:51:07 +0900

    예시 추가

--- cogs/Example.py
+++ cogs/Example.py
@@ -2,6 +2,48 @@ from discord.ext import commands
 from discord import app_commands
 import discord
 
+class ButtonExample(discord.ui.View):
+    def __init__(self, ctx):
+        super().__init__(timeout=150)
+        self.ctx = ctx
+    
+    @discord.ui.button(label="예시버튼", style=discord.ButtonStyle.gray)
+    async def example_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.userid == interaction.user.id:
+            print(1)
+        else:
+            pass
+    
+    async def on_timeout(self) -> None:
+        try:
+            await self.message.edit(content="타임 아웃",view=None)
+        except:
+            pass
+class ModalExample(discord.ui.Modal, title="모달 제목"):
+    input_example = discord.ui.TextInput(label="입력하세요.", style=discord.TextStyle.short)
+    async def on_submit(self, interaction: discord.Interaction):
+        print(1)
+
+class SelectOptionExample(discord.ui.View):
+    def __init__(self, ctx):
+        super().__init__()
+        self.ctx = ctx
+
+        self.select = discord.ui.Select(
+            placeholder="예 혹은 아니요를 선택하세요",
+            options=[
+                discord.SelectOption(label="1", description="설명", value="1"),
+                discord.SelectOption(label="2", description="설명",value="2")
+            ])
+        self.add_item(self.select)
+        self.select.callback = self.callback_selectoption
+
+    async def callback_selectoption(self, interaction: discord.Interaction):
+        if self.userid == interaction.user.id:
+            print(1)
+        else:
+            pass
+
 class Example(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
---
+++ main.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands, tasks
+from discord import app_commands
+import asyncio
+import os
+
+# Intents 객체 생성
+intents = discord.Intents.default()
+
+# 모든 이벤트 활성화
+intents.all()
+
+# 봇 클라이언트 생성 시 intents 옵션으로 intents 전달
+bot = commands.Bot(command_prefix='.', intents=intents, application_id='id')
+
+@bot.event
+async def on_ready():
+    print('Online.')
+
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+    if message[0] == '.':
+        return
+    if message.author == bot.user:
+        return
+    await message.channel.send("hello")
+    
+async def load():
+    for file in os.listdir('./cogs'):
+        if file.endswith('.py'):
+            await bot.load_extension(f'cogs.{file[:-3]}')
+
+async def main():
+    await load()
+    await bot.start('token')
+
+asyncio.run(main())
